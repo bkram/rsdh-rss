3be6be36d174309d91561ce787d849070556e17e
Mark de Bruijn <mrdebruijn@gmail.com>
2020-01-19 11:08:50 +0100

    Add some more output, auto create directory

--- rsdh-rss.py
+++ rsdh-rss.py
@@ -13,8 +13,10 @@ from stack import Stack
 
 
 def getargs():
-    parser = argparse.ArgumentParser(description='RSDH RSS feed generator.')
-    parser.add_argument('-c', '--config', help='Configuration file', dest='config', required=True)
+    parser = argparse.ArgumentParser(description="RSDH RSS feed generator.")
+    parser.add_argument(
+        "-c", "--config", help="Configuration file", dest="config", required=True
+    )
     args = parser.parse_args()
     return args
 
@@ -23,32 +25,45 @@ def getconfig(args):
     config = configparser.ConfigParser()
     try:
         config.read(args.config)
-        stacksite = config.get('site', 'stacksite')
-        token = config.get('site', 'token')
-        showdir = config.get('show', 'showdir')
-        title = config.get('show', 'title')
-        link = config.get('show', 'link')
-        description = config.get('show', 'description')
-        language = config.get('show', 'language')
-        image = config.get('show', 'image')
-        latestmatch = config.get('show', 'latestmatch')
-        output = config.get('file', 'output')
-        localdir = config.get('local', 'location')
-        download = config.get('download', 'location')
-        return stacksite, token, showdir, title, link, description, language, \
-               image, latestmatch, output, localdir, download
+        stacksite = config.get("site", "stacksite")
+        token = config.get("site", "token")
+        showdir = config.get("show", "showdir")
+        title = config.get("show", "title")
+        link = config.get("show", "link")
+        description = config.get("show", "description")
+        language = config.get("show", "language")
+        image = config.get("show", "image")
+        latestmatch = config.get("show", "latestmatch")
+        output = config.get("file", "output")
+        localdir = config.get("local", "location")
+        download = config.get("download", "location")
+        return (
+            stacksite,
+            token,
+            showdir,
+            title,
+            link,
+            description,
+            language,
+            image,
+            latestmatch,
+            output,
+            localdir,
+            download,
+        )
     except Exception as e:
-        exit('Configuration {}\n please check config file'.format(e))
+        exit("Configuration {}\n please check config file".format(e))
 
 
 def createitem(filename, title, pubdate, url, size, mediatype, permalink):
-    guid = hashlib.md5(filename.encode('utf-8')).hexdigest().upper()
+    guid = hashlib.md5(filename.encode("utf-8")).hexdigest().upper()
     item = rfeed.Item(
         title=title,
         pubDate=pubdate,
         description=title,
         enclosure=rfeed.Enclosure(url=url, length=size, type=mediatype),
-        guid=rfeed.Guid(guid, isPermaLink=permalink))
+        guid=rfeed.Guid(guid, isPermaLink=permalink),
+    )
     return item
 
 
@@ -60,60 +75,72 @@ def createfeed(rss, title, description, language, image, link):
         language=language,
         lastBuildDate=datetime.datetime.now(),
         items=rss,
-        image=rfeed.Image(
-            url=image, link=link, title=title))
+        image=rfeed.Image(url=image, link=link, title=title),
+    )
     return feed.rss()
 
 
 def downloadurl(showdir, path, download):
-    url = '{}{}/{}'.format(
-        download, showdir.replace(" ", "%20"), path.split('/')[-1])
+    url = "{}{}/{}".format(download, showdir.replace(" ", "%20"), path.split("/")[-1])
     return url
 
 
 def downloadfile(rsdh, localdir, path):
-    filename = '{}/{}'.format(localdir, path.split('/')[-1])
+    filename = "{}/{}".format(localdir, path.split("/")[-1])
+    if not Path(localdir).is_dir():
+        Path(localdir).mkdir()
     if not Path(filename).is_file():
-        f = open(filename, mode='wb')
+        print('Downloading to disk from "{}" to "{}'.format(path, filename))
+        f = open(filename, mode="wb")
         f.write(rsdh.download(path))
         f.close()
+    else:
+        print('File already on disk "{}"'.format(path))
 
 
 def detectdate(description):
     airdate = None
-    for item in description.replace('_', ' ').split(' '):
-        if len(item) == 8 and item.startswith('20'):
+    for item in description.replace("_", " ").replace("+", " ").split(" "):
+        if len(item) == 8 and item.startswith("20"):
             try:
-                airdate = datetime.datetime.strptime(item, '%Y%m%d')
+                airdate = datetime.datetime.strptime(item, "%Y%m%d")
             except Exception as e:
-                print('Error: {} : cannot convert {} into datetime object [{}]'.
-                      format(description, item, e))
+                print(
+                    "Error: {} : cannot convert {} \
+                     into datetime object [{}]".format(
+                        description, item, e
+                    )
+                )
     return airdate
 
 
 def getshows(rsdh, showdir, localdir, download, permalink, **kwargs):
     rss = []
     dirlist = []
-    match = ''
-    maxcount = 10
+    match = ""
+    maxcount = 5
     scount = 0
-    if 'match' in kwargs:
-        match = kwargs['match']
+    if "match" in kwargs:
+        match = kwargs["match"]
     for i in rsdh.list(directory=showdir):
-        if i['Type'] == 'Dir':
-            dirlist.append(i['Name'])
+        if i["Type"] == "Dir":
+            dirlist.append(i["Name"])
     if not len(dirlist):
         dirlist.append(showdir)
     for shows in dirlist:
         for show in rsdh.list(directory=shows):
-            if scount < maxcount and show['Type'] == 'audio/mpeg' and match in show['Path']:
-                filename = show['Path'].split('/')[-1]
-                filesize = show['Size']
-                filetype = show['Mediatype']
-                description = filename.split('.')[0].replace('-', ' ')
+            if (
+                scount < maxcount
+                and show["Type"] == "audio/mpeg"
+                and match in show["Path"]
+            ):
+                filename = show["Path"].split("/")[-1]
+                filesize = show["Size"]
+                filetype = show["Mediatype"]
+                description = filename.split(".")[0].replace("-", " ")
                 showdate = detectdate(description)
-                fileurl = downloadurl(localdir, show['Path'], download)
-                downloadfile(rsdh, localdir, show['Path'])
+                fileurl = downloadurl(localdir, show["Path"], download)
+                downloadfile(rsdh, localdir, show["Path"])
                 rss.append(
                     createitem(
                         filename,
@@ -122,20 +149,36 @@ def getshows(rsdh, showdir, localdir, download, permalink, **kwargs):
                         fileurl,
                         filesize,
                         filetype,
-                        permalink=permalink))
+                        permalink=permalink,
+                    )
+                )
+                print("Adding item {}".format(filename))
                 scount += 1
     return rss
 
 
 def main():
     args = getargs()
-    stacksite, token, showdir, title, link, description, language, image, \
-    latestmatch, output, localdir, download = getconfig(args)
+    (
+        stacksite,
+        token,
+        showdir,
+        title,
+        link,
+        description,
+        language,
+        image,
+        latestmatch,
+        output,
+        localdir,
+        download,
+    ) = getconfig(args)
     rsdh = Stack(stacksite=stacksite, token=token)
-    rssallshows = getshows(rsdh, showdir, localdir, download, permalink=True )
-    rsslatest = getshows(rsdh, '/- LATEST RECORDINGS/', localdir, download, permalink=True, match=latestmatch)
-    f = open(output, 'w')
-    f.write(createfeed(rsslatest + rssallshows, title, description, language, image, link))
+    rssallshows = getshows(
+        rsdh, showdir, localdir, download, permalink=True, match=latestmatch
+    )
+    f = open(output, "w")
+    f.write(createfeed(rssallshows, title, description, language, image, link))
     f.close()
 
 
